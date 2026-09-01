c227fd06a7ab84bb8e60622c47204ec48b5607cb
Julian Morales <julian.morales-z@mail.escuelaing.edu.co>
2026-09-01 13:04:26 -0500

    feat: estructura Maven, carpetas docs y README con evidencias

---
+++ src/test/java/edu/dosw/parcial/AppTest.java
@@ -0,0 +1,11 @@
+package edu.dosw.parcial;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
